ed93852d0ed1c048c580b4ef96e1de78b1054d8d
Kennie Harold <kennie.harold@gmail.com>
2021-09-28 23:44:10 +0800

    Refactored skills

--- src/sections/Skills/index.js
+++ src/sections/Skills/index.js
@@ -1,45 +1,13 @@
 import React from "react";
 import { Grid, Box, Paper, Typography } from "@material-ui/core";
-import { ThemeProvider, makeStyles } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/core/styles";
 import SectionTitle from "../../components/SectionTitle";
-import LinearProgressWithLabel from "../../components/LinearProgressWithLabel";
 import { Icon } from "@iconify/react";
 import { colorTheme } from "../../styles/themes";
 import "./styles.css";
 import skillItems from "./skillItems";
 
-const useStyles = makeStyles({
-  skillItemPaper: {
-    width: screen.width / 2.8,
-    minHeight: screen.height / 3.4,
-    margin: 20,
-    display: "flex",
-    flexDirection: "column",
-  },
-  skillIconContainer: {
-    margin: 15,
-  },
-  skillItemTitle: {
-    padding: 15,
-    //marginBottom: 15,
-  },
-  skillItemTitleText: {
-    fontWeight: 700,
-  },
-  skillItemTitleIcon: {
-    fontSize: 35,
-    marginRight: 10,
-    color: "#41A6E7",
-  },
-  skillItemIconName: {
-    fontWeight: 700,
-    marginTop: 2,
-  },
-});
-
 const Skills = () => {
-  const classes = useStyles();
-
   return (
     <section id="skills">
       <ThemeProvider theme={colorTheme}>
@@ -62,91 +30,27 @@ const Skills = () => {
               title="skills"
             />
           </Box>
-          <Box
-            id="skills__grid__content"
-            display="flex"
-            justifyContent="center"
-            alignContent="center"
-            alignItems="center"
-            flexWrap="wrap"
-            flex={9}
-          >
+          <Grid container>
             {skillItems.map((skillItem) => (
-              <Paper
-                id="skill--paper"
+              <Grid
                 key={skillItem.key}
-                className={classes.skillItemPaper}
+                container
+                item
+                lg={3}
+                md={4}
+                sm={6}
+                xs={12}
               >
-                <LinearProgressWithLabel
-                  color="secondary"
-                  value={skillItem.rate}
-                />
-                <Box
-                  display="flex"
-                  flex={1}
-                  width="100%"
-                  flexDirection="column"
-                >
-                  <Box
-                    display="flex"
-                    alignItems="center"
-                    className={classes.skillItemTitle}
-                    flex={1}
-                  >
-                    <Icon
-                      id="skill--title-icon"
-                      icon={skillItem.titleIcon}
-                      className={classes.skillItemTitleIcon}
-                    />
-                    <Typography
-                      id="skill--title"
-                      variant="h6"
-                      color="textSecondary"
-                      className={classes.skillItemTitleText}
-                    >
-                      {skillItem.title}
-                    </Typography>
-                  </Box>
-                  <Box
-                    display="flex"
-                    justifyContent="space-evenly"
-                    alignContent="space-evenly"
-                    alignItems="center"
-                    flexWrap="wrap"
-                    flex={9}
-                    style={{ marginTop: -20 }}
-                  >
-                    {skillItem.toolsList.map((tool) => (
-                      <Box
-                        className={classes.skillIconContainer}
-                        key={tool.key}
-                        display="flex"
-                        flexDirection="column"
-                        justifyContent="center"
-                        alignItems="center"
-                      >
-                        <Icon
-                          id="skill--icon"
-                          icon={tool.icon}
-                          style={{
-                            fontSize: 50,
-                            color: tool.name === "Gspread" ? "#0F9855" : null,
-                          }}
-                        />
-                        <Typography
-                          variant="caption"
-                          color="textSecondary"
-                          className={classes.skillItemIconName}
-                        >
-                          {tool.name}
-                        </Typography>
-                      </Box>
-                    ))}
-                  </Box>
-                </Box>
-              </Paper>
+                <Paper id="skill-card" key={skillItem.key}>
+                  <Icon
+                    icon={skillItem.icon}
+                    style={{ fontSize: 50, ...skillItem.styles }}
+                  />
+                  <Typography variant="h6">{skillItem.name}</Typography>
+                </Paper>
+              </Grid>
             ))}
-          </Box>
+          </Grid>
         </Grid>
       </ThemeProvider>
     </section>
--- src/sections/Skills/skillItems.js
+++ src/sections/Skills/skillItems.js
@@ -1,122 +1,94 @@
-import appIcon from "@iconify/icons-ls/app";
 import fileTypeNode from "@iconify/icons-vscode-icons/file-type-node";
 import firebaseIcon from "@iconify/icons-logos/firebase";
 import reactIcon from "@iconify/icons-logos/react";
 import reduxIcon from "@iconify/icons-logos/redux";
-import laptopCode from "@iconify/icons-fa-solid/laptop-code";
 import fileTypeCss from "@iconify/icons-vscode-icons/file-type-css";
-import designIcon from "@iconify/icons-whh/design";
-import figmaIcon from "@iconify/icons-logos/figma";
-import fileTypePhotoshop from "@iconify/icons-vscode-icons/file-type-photoshop";
-import adobeXd from "@iconify/icons-file-icons/adobe-xd";
-import brushIcon from "@iconify/icons-fa-solid/brush";
-import seleniumIcon from "@iconify/icons-logos/selenium";
 import pythonIcon from "@iconify/icons-logos/python";
-import googlesheetsIcon from "@iconify/icons-simple-icons/googlesheets";
 import shopifyIcon from "@iconify/icons-logos/shopify";
+import mongodbIcon from "@iconify/icons-simple-icons/mongodb";
+import fileTypeLightSolidity from "@iconify/icons-vscode-icons/file-type-light-solidity";
 
 const skillItems = [
   {
-    key: "skillItem-mobileAppDevelopment",
-    title: "Mobile App Development",
-    rate: 80,
-    titleIcon: appIcon,
-    toolsList: [
-      {
-        key: "mobileAppDevelopment-tool-firebase",
-        icon: firebaseIcon,
-        name: "Firebase",
-      },
-      {
-        key: "mobileAppDevelopment-tool-redux",
-        icon: reduxIcon,
-        name: "Redux",
-      },
-      {
-        key: "mobileAppDevelopment-tool-node",
-        icon: fileTypeNode,
-        name: "Node",
-      },
-      {
-        key: "mobileAppDevelopment-tool-react",
-        icon: reactIcon,
-        name: "React Native",
-      },
-    ],
+    key: "skill-react",
+    icon: reactIcon,
+    name: "React",
+    styles: {
+      marginRight: 15,
+    },
   },
   {
-    key: "skillItem-webDevelopment",
-    title: "Web Development",
-    rate: 75,
-    titleIcon: laptopCode,
-    toolsList: [
-      {
-        key: "webDevelopment-tool-react",
-        icon: reactIcon,
-        name: "React",
-      },
-      {
-        key: "webDevelopment-tool-node",
-        icon: fileTypeNode,
-        name: "Node",
-      },
-      {
-        key: "webDevelopment-tool-css",
-        icon: fileTypeCss,
-        name: "CSS",
-      },
-      {
-        key: "webDevelopment-shopify",
-        icon: shopifyIcon,
-        name: "Shopify",
-      },
-    ],
+    key: "skill-redux",
+    icon: reduxIcon,
+    name: "Redux",
+    styles: {
+      marginRight: 18,
+    },
   },
   {
-    key: "skillItem-designing",
-    title: "Designing",
-    rate: 50,
-    titleIcon: designIcon,
-    toolsList: [
-      {
-        key: "designing-tool-figma",
-        icon: figmaIcon,
-        name: "Figma",
-      },
-      {
-        key: "designing-tool-ps",
-        icon: fileTypePhotoshop,
-        name: "Adobe Photoshop",
-      },
-      {
-        key: "designing-tool-xd",
-        icon: adobeXd,
-        name: "Adobe XD",
-      },
-    ],
+    key: "skill-node",
+    icon: fileTypeNode,
+    name: "Node.js",
+    styles: {
+      marginRight: 15,
+    },
   },
   {
-    key: "skillItem-webScraping",
-    title: "Web Scraping",
-    rate: 55,
-    titleIcon: brushIcon,
-    toolsList: [
-      {
-        key: "webScraping-tool-selenium",
-        icon: seleniumIcon,
-        name: "Selenium",
-      },
-      {
-        key: "webScraping-tool-python",
-        icon: pythonIcon,
-        name: "Python",
-      },
-      {
-        key: "webScraping-tool-gspread",
-        icon: googlesheetsIcon,
-        name: "Gspread",
-      },
-    ],
+    key: "skill-mongodb",
+    icon: mongodbIcon,
+    name: "MongoDB",
+    styles: {
+      marginRight: 7,
+      color: "#0FA24C",
+    },
+  },
+  {
+    key: "skill-react-native",
+    icon: reactIcon,
+    name: "React Native",
+    styles: {
+      marginRight: 15,
+    },
+  },
+  {
+    key: "skill-firebase",
+    icon: firebaseIcon,
+    name: "Firebase",
+    styles: {
+      marginRight: 20,
+    },
+  },
+  {
+    key: "skill-css",
+    icon: fileTypeCss,
+    name: "CSS",
+    styles: {
+      marginRight: 15,
+    },
+  },
+  {
+    key: "skill-shopify",
+    icon: shopifyIcon,
+    name: "Shopify",
+    styles: {
+      marginRight: 20,
+    },
+  },
+  {
+    key: "skill-python",
+    icon: pythonIcon,
+    name: "Python",
+    styles: {
+      marginRight: 20,
+    },
+  },
+  {
+    key: "skill-solidity",
+    icon: fileTypeLightSolidity,
+    name: "Solidity",
+    styles: {
+      marginRight: 15,
+    },
   },
 ];
 
